4973fbc91634809f7e1b7c645fed788124a95863
Eduardo Guette <eduardoguette@github.com>
2020-07-10 00:04:08 +0200

    1/3

---
+++ src/main.js
@@ -0,0 +1,23 @@
+const share = document.querySelector("#foo-perfil");
+
+share.addEventListener("click", function (e) {
+  if (document.querySelector("#foo-share").classList.contains("is-hidden")) {
+    document.querySelector("#foo-share").classList.remove("is-hidden");
+    document.querySelector('#foo-perfil').classList.add('is-hidden')
+  } else {
+    document.querySelector("#foo-share").classList.add("is-hidden");
+    document.querySelector('#foo-perfil').classList.remove('is-hidden')
+  }
+});
+
+
+const share1 = document.querySelector("div.container_share");
+share1.addEventListener("click", function (e) {
+  if (document.querySelector("#foo-share").classList.contains("is-hidden")) {
+    document.querySelector("#foo-share").classList.remove("is-hidden");
+    document.querySelector('#foo-perfil').classList.add('is-hidden')
+  } else {
+    document.querySelector("#foo-share").classList.add("is-hidden");
+    document.querySelector('#foo-perfil').classList.remove('is-hidden')
+  }
+});
